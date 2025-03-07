6a477ccef8778a2dc5935f8b260292d051800dca
adamhorvitz <149023194+adamhorvitz@users.noreply.github.com>
2025-03-07 14:14:39 -0500

    Swift project setup

---
+++ StepQuestSwift/StepQuestSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StepQuestSwift
+//
+//  Created by Adam Horvitz on 3/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepQuestSwift/StepQuestSwift/StepQuestSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepQuestSwiftApp.swift
+//  StepQuestSwift
+//
+//  Created by Adam Horvitz on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct StepQuestSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
